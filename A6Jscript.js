8cb3456f4b84d098e773a8e288c456900fb3c31e
Josh <joshua_blanchette@student.uml.edu>
2014-12-03 10:24:23 -0500

    Update A6Jscript.js

--- A6Jscript.js
+++ A6Jscript.js
@@ -1,43 +1,128 @@
 /*
-- Assignment 6: Creating an Interactive Dynamic Table
+- Assignment 8: Creating an Interactive Dynamic Table With Tabs
   - Joshua Blanchette, UMass Lowell Computer Science Transfer Student
   - Email:  Joshua_Blanchette@student.uml.edu
   - Date: 10/22/2014
   - This file is my JavaScript that actually creates the table, and checks for user
   - error.  It takes the given information from the user and multiplies the numbers
-  - accordingly.  
+  - accordingly.  Enhanced: You can now create more than one multiplication table via tabs.
 */
 $(document).ready(function() {
-  $("#form1").submit(function() {
-    $("table").remove();  //This removes the table when another one is generated
-    $("h2").remove(); // Removes previous error messages
-      //Below are declerations of multiple variables.
-      var RowStart = Number(document.getElementById('StartH').value);
-      var RowEnd = Number(document.getElementById('EndH').value);
-      var ColumnStart = Number(document.getElementById('StartV').value);
-      var ColumnEnd = Number(document.getElementById('EndV').value);
+//Variable definitions below handle the creation of tabs and assignment of tabs
+  $("#tabs").tabs();
+  var tabsdiv = $("#tabs");
+  var tabslist = tabsdiv.find("ul");
+  var nextTabNo = tabslist.find("li").length(); //keeps track of number of tabs
+    
+  var AddTabButtonClickHandler = function() {
       
-      var n1= document.querySelector("#StartH");
-      var n2= document.querySelector("#EndH");
-      var n3= document.querySelector("#StartV");
-      var n4= document.querySelector("#EndV");
-      str = "";
-      //Below sets the background color of my text boxes to white
-      n1.style.backgroundColor="White";
-      n2.style.backgroundColor="White";
-      n3.style.backgroundColor="White";
-      n4.style.backgroundColor="White";
+    //Below creates a new tab. 97 is the value of char A.  .fromCharCode converts to char.
+    tabslist.append( '<li><a href="#' + String.fromCharCode( 97+nextTabNo+1) + '">' + 
+    'Tab' + (nextTabNo+1) + '</a></li>' );
+    
+    //Below adds the content to the new tab that was created.
+    tabsdiv.append( '<div id="' + String.fromCharCode(97+nextTabNo) + '">');
+    $("#" + String.fromCharCode( 97+nextTabNo) ).append( '<button class="removeTab">Remove Tab</button>' ) ;
+    
+    //Refresh tab structure so newly added components appear.
+    $("#tabs").tabs("refresh");
+    
+    //These two lines come after the tab refresh.  Buttons must exisit after refresh
+    $("#" + String.fromCharCode( 97+nextTabNo ) + " .addTab").click( AddTabButtonClickHandler ) ;
+    $("#" + String.fromCharCode( 97+nextTabNo ) + " .removeTab").click( RemoveTabButtonClickHandler ) ;
+    
+    //Most important part, you MUST increment the tab counter.
+    nextTabNo++;
+  }; //End of AddTab function
+    
+    
+  var RemoveTabButtonClickHandler = function() {
+    // remove tab content
+    $(this).parent().remove() ;
+    
+    // remove tab itself        
+    var id = $(this).parent().attr("id") ;
+    var tabToRemove = tabslist.find( "li a[href='#" + id + "']").parent() ;
+    tabToRemove.remove() ;
+    
+    // refresh the tab structure to make the newly added components appear
+    $("#tabs").tabs( "refresh" ) ;
+
+  }; //End of RemoveTab function
+  
+  // add the AddTab button click handler to all AllTab buttons
+  $(".addTab").click( AddTabButtonClickHandler ) ;
+  
+  // add the AddTab button click handler to all AllTab buttons
+  $(".removeTab").click( RemoveTabButtonClickHandler ) ;
+  
+  
+  
+  
+  
+  
+  
+  
+  //All form validation below
+  $("#form1").validate({
+    errorClass: "errorClass",
+      rules: {
+        startH:{
+        number: true,
+        required: true
+               },
+        endH:  {
+        number: true,
+        required: true
+               },
+        startV:{
+        number: true,
+        required: true
+               },
+        endV:  {
+        number: true,
+        required: true
+               }
+             }
+  });  //End of form validation
+    
+    
+  //removes all tabs
+  $("#removeAll").unbind().click(function(){
+  $(".removeTab").click();
+  }); 
+  
+  
+    $("#form1").submit(function() {
+      $("table").remove();//This removes the table when another one is generated
+      $("h2").remove();
+        var RowStart = Number(document.getElementById('StartH').value);
+        var RowEnd = Number(document.getElementById('EndH').value);
+        var ColumnStart = Number(document.getElementById('StartV').value);
+        var ColumnEnd = Number(document.getElementById('EndV').value);
+      
+        var n1= document.querySelector("#StartH");
+        var n2= document.querySelector("#EndH");
+        var n3= document.querySelector("#StartV");
+        var n4= document.querySelector("#EndV");
+      
+        str = "";
+        n1.style.backgroundColor="White";
+        n2.style.backgroundColor="White";
+        n3.style.backgroundColor="White";
+        n4.style.backgroundColor="White";  
         // For the following three if statements generally do that same thing.
-        // They all check if the user inputed incorrect data.  If so, the
-        // text boxes that are incorrect turn red and an error message appears. 
+        // They all checks if the user inputed incorrect data.  If so, the
+        // Text boxes that are incorrect turn red and an error message appears. 
         if(RowStart > RowEnd && ColumnStart > ColumnEnd){
           $("h2").remove();
+          
           n1.style.backgroundColor="Red";
           n2.style.backgroundColor="Red";
           n3.style.backgroundColor="Red";
           n4.style.backgroundColor="Red";
           str +="<h2 style=\"color:red\"> Both your starting values are greater than both your ending values.<br>Please recalculate and try again. </h2>";
-          $(this).append(str);//append all the strings in the str variable. 
+          $(this).append(str);
           return;
         }
         else if(RowStart > RowEnd){
@@ -58,9 +143,8 @@ $(document).ready(function() {
           return;
         }
         
-        $("h2").remove();
-        str += "<table border=2 width=100%>"; 
-        str += "<tr><td> Q </td>";  // This Q appears in the top right block that is not used
+        str += "<table border=2 width=100%>";
+        str += "<tr><td> Q </td>";
         // A for loop to generate the first row in the table
         for(i=RowStart; i<= RowEnd; i++){
           str+="<th>" + i + "</th>";
@@ -74,11 +158,11 @@ $(document).ready(function() {
           for(j=RowStart; j<=RowEnd; j++){
             str+= "<td>" + i*j + "</td>";
           }// End of nested for loop
+          str += "</tr>";
         }// End of for loop 
-        str += "</tr>"; //close tag
-        str += "</table>"; //close tag
-        
-        $(this).append(str); //append all the strings in the str variable. 
+        str += "</table>";
+        //append html code
+        $("#" + String.fromCharCode( 97+nextTabNo-1) ).append(str);
         return;
     });
 });
